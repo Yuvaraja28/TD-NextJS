c2310206ebc95feff0d8951529ff55277eb63e1f
Yuvaraja <64340067+Yuvaraja28@users.noreply.github.com>
2023-01-16 00:41:25 +0530

    Delete pages/api/efficiency_calc directory

--- pages/api/efficiency_calc/diesel.js
+++
@@ -1,7 +0,0 @@
-const GAMMA = 1.4
-
-export default function handler(req, res) {
-  if ((req.query.cut_off_ratio == null) || (req.query.compression_ratio == null)) { return res.status(200).send(0) }
-  return res.status(200).send(((1-((Math.pow(req.query.cut_off_ratio,GAMMA)-1)/((GAMMA*Math.pow(req.query.compression_ratio,(GAMMA-1)))*(req.query.cut_off_ratio-1))))*100).toFixed(2))
-}
-
--- pages/api/efficiency_calc/otto.js
+++
@@ -1,6 +0,0 @@
-const GAMMA = 1.4
-
-export default function handler(req, res) {
-  if (req.query.compression_ratio == null) { return res.status(200).send(0) }
-  return res.status(200).send(((1-(1/(Math.pow(req.query.compression_ratio,(GAMMA-1)))))*100).toFixed(2))
-}

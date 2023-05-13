7f576ad0864efa7540f867b3f8c9cfc5515d399d
Yuvaraja <64340067+Yuvaraja28@users.noreply.github.com>
2023-05-14 02:23:30 +0530

    Add files via upload

--- app/cgpa/page.jsx
+++ app/cgpa/page.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 export default function CGPA() {
     const [cgpa, Cgpa] = useState()
+    const [subjectCount, setSubjectCount] = useState(9)
     async function Calculate() {
       let datas = []
       let total = 0
@@ -9,13 +10,17 @@ export default function CGPA() {
       let input_array = document.getElementsByClassName("full-problem-box")
       for (var i = 0; i < input_array.length; i++) {
         const input = input_array[i].getElementsByClassName('full-problem-input')
-        datas = [...datas, { gp: parseInt(input.gp.value), cp: parseInt(input.cp.value) }]
+        if ((input.gp.value !== '') && (input.cp.value !== '')) {
+          datas = [...datas, { gp: parseInt(input.gp.value), cp: parseInt(input.cp.value) }]
+        }
       }
       datas.forEach(data => {
         total += (data.gp * data.cp)
         remainder += data.cp
       })
-      Cgpa(`Calculated CGPA is ${(total/remainder).toFixed(4)}`)
+      if (!((total == 0) && (remainder == 0))) {
+        Cgpa(`Calculated CGPA is ${(total/remainder).toFixed(4)}`)
+      }
     }
     async function Clear() {
         let input_array = document.getElementsByClassName("full-problem-input")
@@ -26,8 +31,8 @@ export default function CGPA() {
           <div className='full-problem-box'>
             <span className='full-problem-label'>Subject {no}</span>
             <div className="full-problem-box-inner">
-              <input required className='full-problem-input' placeholder="Grade Point" name="gp" type="number" step="0.1" />
-              <input required className='full-problem-input' placeholder="Credit Point" name="cp" type="number" step="0.1" />
+              <input required className='full-problem-input' placeholder="Grade Point" name="gp" type="number" step="1" />
+              <input required className='full-problem-input' placeholder="Credit Point" name="cp" type="number" step="1" />
             </div>
           </div>
         )
@@ -35,8 +40,9 @@ export default function CGPA() {
     return (
       <div className="mainframe">
         <span className="headline">CGPA Calculator</span>
+        <input style={{ marginTop: '-0.1em', marginBottom: '0.3em' }} onChange={(s) => {if ((parseInt(s.currentTarget.value) > 0) && (s.currentTarget.value !== '') && (parseInt(s.currentTarget.value) <= 10)) {setSubjectCount(parseInt(s.currentTarget.value))}}} className='full-problem-input' placeholder="Total Subjects" name="subs" type="number" step="1" />
         <div className="full-problem-container-multiple">
-          {[1, 2, 3, 4, 5, 6 ,7, 8, 9].map(no => <Subject key={no} no={no} />)}
+          {Array(subjectCount).fill(0).map((e, i)=> i+1).map(no => <Subject key={no} no={no} />)}
         </div>
         <div className="full-problem-container-single">
           <div className="full-problem-box-inner">
